09b58bab94ea6b650e2c693c077f1283c22c0488
chris westerduin <chriswesterduin@outlook.com>
2021-03-05 12:05:50 +0000

    add file structure

---
+++ server/models/films.js
@@ -0,0 +1,37 @@
+const filmData = require('../data');
+
+class Film {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.director = data.director;
+    }
+
+    static get all() {
+        const films = filmsData.map((film) => new Film(film));
+        return films;
+    }
+
+    static findById(id) {
+        try {
+            const filmData = filmData.filter((film) => film.id === id)[0];
+            const film = new Film(filmData);
+            return film;
+        } catch (err) {
+            throw new Error('We don\'t have that film');
+        }
+    }
+
+    static create(film) {
+        const newFilmId = filmsData.length + 1;
+        const newFilm = new Film({ id: newFilmId, ...film });
+        filmsData.push(newFilm);
+        return newFilm;
+    }
+
+    destroy() {
+
+    }
+}
+
+module.exports = Film;
